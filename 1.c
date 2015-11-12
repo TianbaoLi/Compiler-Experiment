cb18e318118b28ea4d2abec2f1e5db03730decf1
TuringMacLee <turingmac@hotmail.com>
2015-11-12 19:05:30 +0800

    Fix lexer error

--- 1.c
+++ 1.c
@@ -3,14 +3,14 @@ int func1 () {
 }
 int main()/*the main function*/
 {
-	int 23a;/*identifier*/
+	int a;
 	float _b;
 	bool c12_354;
 	int D[10];
 	float e;
 	c = 1e-8;
 	if(c > 0) {
-		a = 1.1.1 + 10;/*wrong float*/
+		a = 1.1 + 10;
 		b = 10.9 + 8.9;
 	}
 	else {
@@ -24,5 +24,5 @@ int main()/*the main function*/
 	c = 8 & 7;
 	e = D[2];
 	func1();
-	string s = "hello world!";
+	/*string s = "hello world!";*/
 }
